e25e4b26cd386bd17daf612003d126c9794fc2f5
Donald Mandra <dmandra@hotmail.com>
2026-02-22 00:06:55 -0500

    Initial Commit

---
+++ Drive for Dana/Drive_for_DanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drive_for_DanaApp.swift
+//  Drive for Dana
+//
+//  Created by Donald Mandra on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Drive_for_DanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
